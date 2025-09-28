e4a65863ced8ff24a906ded43a769a9afdb77404
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-09-28 22:04:31 +0530

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,67 @@
+/*
+
+This is Chapter: Three, in which I am going to learn about Data Mqnipulation Language (DML) which is used for inserting, updating and deleting data in database tables.
+
+We will learn the following commands:
+     1. INSERT - Adding Data to Tables
+     2. UPDATE - Modifying Existing Data
+     3. DELETE - Removing Data from Tables
+
+*/
+
+/*
+----------------------------------------
+               INSERT
+----------------------------------------
+*/
+
+--Method 1: Manual INSERT using VALUES 
+--Insert new records into the customers table
+INSERT INTO customers (id, first_name, country, score)
+VALUES 
+    (6, 'Anna', 'USA', NULL),
+    (7, 'Sam', NULL, 100)
+
+--Method 2: INSERT DATA USING SELECT - Moving Data From One Table to Another 
+-- Copy data from the 'customers' table into 'persons'
+INSERT INTO persons (id, person_name, birth_date, phone)
+SELECT
+    id,
+    first_name,
+    NULL,
+    'Unknown'
+FROM customers
+
+/*
+----------------------------------------
+               UPDATE
+----------------------------------------
+*/
+
+-- Change the score of customer with ID 6 to 0
+UPDATE customers
+SET score = 0
+WHERE id = 6
+
+-- Change the score of customer with ID 10 to 0 and update the country to 'UK'
+UPDATE customers
+SET score = 0,
+    country = 'UK'
+WHERE id = 10
+
+-- Update all customers with a NULL score by setting their score to 0
+UPDATE customers
+SET score = 0
+WHERE score IS NULL
+
+-- Verify the update
+SELECT *
+FROM customers
+WHERE score IS NULL
+
+
+/*
+----------------------------------------
+               DELETE
+----------------------------------------
+*/
