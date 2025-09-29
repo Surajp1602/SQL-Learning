c6d9bc6cecb6d6fbd9703d91effaff7c80b0a06c
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-09-29 22:04:32 +0530

    Update 04_FILTERING_DATA.sql

--- 04_FILTERING_DATA.sql
+++ 04_FILTERING_DATA.sql
@@ -76,15 +76,49 @@ WHERE NOT score < 500
 ----------------------------------------
 */
 
+-- Retrieve all customers whose score falls in the range between 100 and 500.
+SELECT *
+FROM customers
+WHERE score BETWEEN 100 AND 500
+
+-- Alternative method (Equivalent to BETWEEN)
+SELECT *
+FROM customers
+WHERE score >= 100 AND score <= 500
+
 /*
 ----------------------------------------
             Set Filtering
 ----------------------------------------
 */
 
+-- Retrieve all customers from either Germany or the USA.
+SELECT *
+FROM customers
+WHERE country IN ('Germany', 'USA')
 
 /*
 ----------------------------------------
            Pattern Matching
 ----------------------------------------
 */
+
+-- Find all customers whose first name starts with 'M'.
+SELECT *
+FROM customers
+WHERE first_name LIKE 'M%'
+
+-- Find all customers whose first name ends with 'n'.
+SELECT *
+FROM customers
+WHERE first_name LIKE '%n'
+
+-- Find all customers whose first name contains 'r'.
+SELECT *
+FROM customers
+WHERE first_name LIKE '%r%'
+
+-- Find all customers whose first name has 'r' in the third position.
+SELECT *
+FROM customers
+WHERE first_name LIKE '__r%'
