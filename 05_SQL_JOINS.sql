10b12b08e12426d024b6fe23ae345c6fe0e447be
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-10-01 00:09:04 +0530

    Add files via upload

---
+++ 05_SQL_JOINS.sql
@@ -0,0 +1,27 @@
+/*
+
+ SQL JOINS
+
+ This is Chapter: 5, in which I am going to learn about one of the most important topics in SQL - JOINS, which are used to combine data from multiple tables.
+
+ The contents:
+	 1. Basic Joins
+        - INNER JOIN
+        - LEFT JOIN
+        - RIGHT JOIN
+        - FULL JOIN
+     2. Advanced Joins
+        - LEFT ANTI JOIN
+        - RIGHT ANTI JOIN
+        - ALTERNATIVE INNER JOIN
+        - FULL ANTI JOIN
+        - CROSS JOIN
+     3. Multiple Table Joins
+
+*/
+
+/*
+--------------------------------------
+            BASIC JOINS
+--------------------------------------
+*/
