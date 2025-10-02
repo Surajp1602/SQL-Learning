3b1ef45235eaba2e9528cc6169489198694de6ee
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-10-02 23:46:44 +0530

    Update 06_SET_OPERATIONS.sql

--- 06_SET_OPERATIONS.sql
+++ 06_SET_OPERATIONS.sql
@@ -135,3 +135,43 @@ SELECT
     FirstName,
     LastName
 FROM Sales.Customers;
+
+/*
+--------------------------------------
+              TASK
+--------------------------------------
+*/
+
+-- Combine order data from Orders and OrdersArchive into one report without duplicates 
+SELECT
+    'Orders' AS SourceTable,
+    OrderID,
+    ProductID,
+    CustomerID,
+    SalesPersonID,
+    OrderDate,
+    ShipDate,
+    OrderStatus,
+    ShipAddress,
+    BillAddress,
+    Quantity,
+    Sales,
+    CreationTime
+FROM Sales.Orders
+UNION
+SELECT
+    'OrdersArchive' AS SourceTable,
+    OrderID,
+    ProductID,
+    CustomerID,
+    SalesPersonID,
+    OrderDate,
+    ShipDate,
+    OrderStatus,
+    ShipAddress,
+    BillAddress,
+    Quantity,
+    Sales,
+    CreationTime
+FROM Sales.OrdersArchive
+ORDER BY OrderID;
