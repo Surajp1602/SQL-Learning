86f69a8c862aba986e2900794a3fa4743da500db
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-10-06 23:31:34 +0530

    Update 10_NULL_FUNCTIONS.sql

--- 10_NULL_FUNCTIONS.sql
+++ 10_NULL_FUNCTIONS.sql
@@ -1,4 +1,4 @@
-/*
+
 SELECT 
 	AVG(ISNULL(Score, 0)) AVG
 FROM Sales.Customers
@@ -16,29 +16,21 @@ SELECT
 	CASE WHEN Score IS NULL THEN 1 ELSE 0 END AS FLAG
 FROM Sales.Customers
 ORDER BY COALESCE(Score,9999999)  
-*/
 
-/*
 SELECT 
 	Sales,
 	Quantity, 
 	Sales / NULLIF(Quantity, 0) AS SalesPrice
 FROM Sales.Orders
-*/
 
-/*
 SELECT *
 FROM Sales.Customers
 WHERE Score IS NULL
-*/
 
-/*
 SELECT *
 FROM Sales.Customers
 WHERE Score IS NOT NULL
-*/
 
-/*
 SELECT 
 	c.*,
 	o.OrderID
@@ -46,4 +38,4 @@ FROM Sales.Customers c
 LEFT JOIN Sales.Orders o
 ON o.CustomerID = c.CustomerID
 WHERE o.CustomerID IS NULL
-*/
+
