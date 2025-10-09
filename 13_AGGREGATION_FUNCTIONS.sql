8a149dfa6a3f2d4b9dacc04954db80505cbc8f45
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-10-09 23:43:30 +0530

    Add files via upload

---
+++ 13_AGGREGATION_FUNCTIONS.sql
@@ -0,0 +1,64 @@
+/*
+
+SQL Aggregation Functions
+
+This is Chapter: Thirteen, in which I am going to learn about various Aggregation Functions.
+
+The contents:
+	1. COUNT
+	2. SUM
+	3. AVG
+	4. MAX
+	5. MIN
+
+*/
+
+/*
+----------------------------------
+             COUNT
+----------------------------------
+*/
+
+-- Find the total number of customers.
+SELECT COUNT(*) AS CustomerCount
+FROM Sales.Customers
+
+/*
+----------------------------------
+              SUM
+----------------------------------
+*/
+
+-- Find the total sales of all orders.
+SELECT SUM(Sales) AS SalesTotal
+FROM Sales.Orders
+
+/*
+----------------------------------
+              AVG
+----------------------------------
+*/
+
+-- Find the average sales of all orders.
+SELECT AVG(Sales) AS SalesAverage
+FROM Sales.Orders
+
+/*
+----------------------------------
+              MAX
+----------------------------------
+*/
+
+-- Find the highest score among the customers.
+SELECT MAX(Score) AS HighestScore
+FROM Sales.Customers
+
+/*
+----------------------------------
+               MIN
+----------------------------------
+*/
+
+-- Find the lowest score among the customers.
+SELECT MIN(Score) AS LowestScore
+FROM Sales.Customers
