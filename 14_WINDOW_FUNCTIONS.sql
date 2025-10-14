40a963e9f0794fe08da4ed94bc329af02959b745
Surajp1602 <108174763+Surajp1602@users.noreply.github.com>
2025-10-14 23:16:14 +0530

    Add files via upload

---
+++ 14_WINDOW_FUNCTIONS.sql
@@ -0,0 +1,28 @@
+/*
+
+WINDOW AGGREGATION
+
+This is Chapter: Fourteen, in which I am going to learn about basics of Window Aggregations.
+
+
+*/
+
+-- Find the total sales across all orders.
+SELECT 
+	SUM(Sales) AS TotalSales
+FROM Sales.Orders
+
+-- Find the total sales for each product.
+SELECT
+	ProductID,
+	SUM(Sales) AS TotalSales
+FROM Sales.Orders
+GROUP BY ProductID
+
+-- Find the total sales for each product, additionally provide details such as order id and order date.
+SELECT
+	OrderID,
+	OrderDate,
+	ProductID,
+	SUM(Sales) OVER(PARTITION BY ProductID) AS TotalSales
+FROM Sales.Orders
